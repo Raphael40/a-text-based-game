fde36664bb591f651789d8b2977559599dea5648
Julian Festing <julfesting@gmail.com>
2023-04-26 20:54:04 +0100

    passed pick up items test

--- lib/player.rb
+++ lib/player.rb
@@ -5,10 +5,22 @@ class Player
 
   def look_around
     return '' if @location.empty?
+
     item = @location[:items]
 
-    location_items = "#{@location[:description]} You see a #{item[0]} on the floor. You see a #{item[1]} on the floor."
+    items_arr = []
+
+    item.each do |item|
+      items_arr << " You see a #{item} on the floor."
+    end
+
+    location_items = "#{@location[:description]}#{items_arr.join}"
 
     return location_items
   end
+
+  def pick_up(item)
+    @location[:items].delete(item)
+  end
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,4 +1,5 @@
 require './lib/player'
+location = {description: "You are in the living-room. A wizard is snoring loudly on the couch.", items: ["whiskey", "bucket"] }
 
 RSpec.describe Player do
   context '.look_around' do
@@ -9,7 +10,6 @@ RSpec.describe Player do
     end
 
     it 'returns the description of the location' do
-      location = { description: "You are in the living-room. A wizard is snoring loudly on the couch.", items: ["whiskey", "bucket"] }
       player = Player.new(location)
 
       expect(player.look_around).to include 'You are in the living-room. A wizard is snoring loudly on the couch.'
@@ -18,14 +18,20 @@ RSpec.describe Player do
     end
 
     it 'returns description and items' do
-      location = {description: "You are in the living-room. A wizard is snoring loudly on the couch.", items: ["whiskey", "bucket"] }
       player = Player.new(location)
 
       expect(player.look_around).to eq 'You are in the living-room. A wizard is snoring loudly on the couch. You see a whiskey on the floor. You see a bucket on the floor.'
     end
   end
 
-  context '.pick_up(item)' do
-    
+  context '.pick_up(item) adds item from location to items array' do
+    it 'removes item from being visible in look around method' do
+      
+      player = Player.new(location)
+      player.look_around
+      player.pick_up('whiskey')
+
+      expect(player.look_around).to eq 'You are in the living-room. A wizard is snoring loudly on the couch. You see a bucket on the floor.'
+    end
   end
 end
